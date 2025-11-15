c2dfeb2cff5b8a06b731aa8c5035163534435500
eodaneze <ezealiokechukwu1999@gmail.com>
2025-11-15 14:17:34 +0100

    basic routing

---
+++ src/user/user.controller.ts
@@ -0,0 +1,44 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+
+
+@Controller('user')
+export class UserController {
+    /* 
+       GET /users
+       GET /users/:id
+       POST /users
+       PATCH /users/:id
+       DELETE /users/:id
+    */
+
+       @Get() //GET /users or /users?role=value
+       findAll(@Query('role') role?: 'INTERN' | 'ADMIN' | 'ENGINEER'){
+          return[]
+       }
+       
+       @Get('interns')
+       findAllInterns(){
+          return []
+       }
+
+       @Get(':id')// GET /users/:id
+       findOne(@Param('id') id: string){
+           return {id}
+       }
+
+       @Post()
+       create(@Body() user:{}){
+         return user
+       }
+
+        @Patch(':id')
+        update(@Param('id') id: string, @Body() userUpdate: {}){
+           return {id, ...userUpdate}
+       }
+
+        @Delete(':id')
+         delete(@Param('id') id: string){
+           return {id}
+       }
+
+}
